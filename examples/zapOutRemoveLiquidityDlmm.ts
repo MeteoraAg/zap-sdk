5a8c1dd23877adbdeed8807c8503610098d95cb4
defi0x1 <cristiando0902@gmail.com>
2025-07-23 13:57:29 +0700

    update example

--- examples/zapOutRemoveLiquidityDlmm.ts
+++ examples/zapOutRemoveLiquidityDlmm.ts
@@ -2,6 +2,7 @@ import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js";
 import { Zap } from "../src/zap";
 import { BN } from "@coral-xyz/anchor";
 import { NATIVE_MINT } from "@solana/spl-token";
+import DLMM, { wrapPosition } from "@meteora-ag/dlmm";
 
 (async () => {
   const wallet = Keypair.fromSecretKey(
@@ -19,12 +20,20 @@ import { NATIVE_MINT } from "@solana/spl-token";
     "BwkWL46z9oJQFebbBr9WpxovjBJs1CZ8PrQdDvd7Xbmw"
   );
 
+  const dlmm = await DLMM.create(connection, poolAddress, {
+    cluster: "devnet",
+  });
+
+  const positionAccount = await connection.getAccountInfo(position);
+
+  const positionState = wrapPosition(dlmm.program, position, positionAccount);
+
   const transaction = await zapSdk.removeDlmmLiquidityWithZapOut({
     user: wallet.publicKey,
     poolAddress,
     position,
-    fromBinId: -356,
-    toBinId: -288,
+    fromBinId: positionState.lowerBinId().toNumber(),
+    toBinId: positionState.upperBinId().toNumber(),
     outputTokenMint: NATIVE_MINT,
     minimumSwapAmountOut: new BN(1),
     bps: new BN(5000),
@@ -34,7 +43,7 @@ import { NATIVE_MINT } from "@solana/spl-token";
   ).blockhash;
   transaction.sign(wallet);
 
-    console.log(await connection.simulateTransaction(transaction));
+  console.log(await connection.simulateTransaction(transaction));
   const signature = await connection.sendRawTransaction(
     transaction.serialize()
   );
