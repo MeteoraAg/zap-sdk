04184d8d324558a47c2d15a7a633687a61b07081
00xSam <sam@raccoons.dev>
2025-08-11 16:10:49 +0800

    test: remove dlmm liquidity and zap out via jup

--- examples/removeDlmmLiquidityAndZapOut.ts
+++ examples/removeDlmmLiquidityAndZapOut.ts
@@ -4,6 +4,7 @@ import {
   Keypair,
   Transaction,
   sendAndConfirmTransaction,
+  SystemProgram,
 } from "@solana/web3.js";
 import BN from "bn.js";
 import { Zap } from "../src/zap";
@@ -11,22 +12,43 @@ import { getJupiterQuote, getJupiterSwapInstruction } from "../src/helpers";
 import DLMM, { getTokenProgramId } from "@meteora-ag/dlmm";
 import { DLMM_PROGRAM_ID } from "../src";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import Decimal from "decimal.js";
+
+export function createJitoTipIx({
+  payer,
+  lamports,
+}: {
+  payer: PublicKey;
+  lamports: string;
+}) {
+  const tipAccount = new PublicKey(
+    "96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5"
+  );
+  return SystemProgram.transfer({
+    fromPubkey: payer,
+    toPubkey: tipAccount,
+    lamports: BigInt(lamports),
+  });
+}
 
 async function main() {
-  const connection = new Connection("https://api.mainnet-beta.solana.com");
+  const connection = new Connection(process.env.RPC_ENDPOINT as string);
 
-  const wallet = Keypair.fromSecretKey(Uint8Array.from([]));
+  const wallet = Keypair.fromSecretKey(
+    bs58.decode(process.env.WALLET_KEYPAIR as string)
+  );
   console.log(`Using wallet: ${wallet.publicKey.toString()}`);
 
   const inputMint = new PublicKey(
-    "BFgdzMkTPdKKJeTipv2njtDEwhKxkgFueJQfJGt1jups"
+    "So11111111111111111111111111111111111111112"
   );
   const outputMint = new PublicKey(
     "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
   );
 
   const lbPairAddress = new PublicKey(
-    "9NRifL3nKQU84hMTbhE7spakkGy5vq4AvNHNQYr8LkW7"
+    "5rCf1DM8LjKTw4YqhnoLcngyZYeNnQqztScTogYHAS6"
   );
 
   const zap = new Zap(connection);
@@ -61,7 +83,9 @@ async function main() {
       }
     }
 
-    const amountXRemoved = totalAmountXRemoved.mul(new BN(1)).div(new BN(100));
+    const amountXRemoved = totalAmountXRemoved
+      .mul(new BN(100))
+      .div(new BN(100));
 
     console.log({
       amountXRemoved: amountXRemoved.toString(),
@@ -83,16 +107,16 @@ async function main() {
           user: wallet.publicKey,
           fromBinId: binIdsToRemove[0],
           toBinId: binIdsToRemove[binIdsToRemove.length - 1],
-          bps: new BN(1 * 100), // Remove 1% of liquidity
+          bps: new BN(100 * 100),
           shouldClaimAndClose: true,
         });
       })
     );
 
-    const transaction = new Transaction();
+    const transaction: Transaction[] = [];
 
     removeLiquidityTxs.forEach((tx) => {
-      transaction.add(...tx);
+      transaction.push(...tx);
     });
 
     let inputTokenProgram = TOKEN_PROGRAM_ID;
@@ -159,6 +183,7 @@ async function main() {
       bestQuoteValue = quotes.dlmm.outAmount;
       bestProtocol = "dlmm";
     }
+    console.log("dmm", bestQuoteValue?.toString());
 
     if (quotes.jupiter?.outAmount) {
       const jupiterAmount = new BN(quotes.jupiter.outAmount);
@@ -167,6 +192,7 @@ async function main() {
         bestProtocol = "jupiter";
       }
     }
+    console.log("jup", bestQuoteValue?.toString());
 
     if (!bestProtocol || !bestQuoteValue) {
       throw new Error("No valid quotes obtained from any protocol");
@@ -212,25 +238,69 @@ async function main() {
       throw new Error(`Invalid protocol selected: ${bestProtocol}`);
     }
 
-    transaction.add(zapOutTx);
+    transaction.push(zapOutTx);
+
+    const res: { landed50: number } = (await fetch(
+      "https://worker.jup.ag/jito-floor"
+    ).then((res) => res.json())) as { landed50: number };
+
+    const jitoFloor = res.landed50;
+
+    console.log(`Jito floor: ${jitoFloor}`);
+
+    const jitoTip = new Decimal(jitoFloor)
+      .mul(10 ** 9)
+      .toDP(0)
+      .toNumber();
+
+    transaction[0].add(
+      createJitoTipIx({ payer: wallet.publicKey, lamports: jitoTip.toString() })
+    );
 
     const { blockhash } = await connection.getLatestBlockhash();
 
-    transaction.recentBlockhash = blockhash;
-    transaction.feePayer = wallet.publicKey;
+    for (const tx of transaction) {
+      tx.recentBlockhash = blockhash;
+      tx.feePayer = wallet.publicKey;
+      tx.sign(wallet);
+    }
 
-    const simulate = await connection.simulateTransaction(transaction);
-    console.log(simulate.value.logs);
+    // const simulate = await connection.simulateTransaction(transaction);
+    // console.log(simulate.value.logs);
 
     console.log("Sending zap transaction...");
-    const signature = await sendAndConfirmTransaction(
-      connection,
-      transaction,
-      [wallet],
-      { commitment: "confirmed" }
-    );
+    const jitoBundleResult: {
+      result: string;
+    } = (await fetch("https://mainnet.block-engine.jito.wtf/api/v1/bundles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-jito-auth": process.env.PRIVATE_JITO_API_KEY,
+      },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        id: 1,
+        method: "sendBundle",
+        params: [
+          transaction.map((signedTx) =>
+            signedTx.serialize().toString("base64")
+          ),
+          { encoding: "base64" },
+        ],
+      }),
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.error(err);
+        return {
+          result: err.message,
+        };
+      })) as {
+      result: string;
+    };
 
-    console.log(`Zap transaction sent: ${signature}`);
+    console.log(jitoBundleResult);
+    console.log(`Zap bundle sent: ${jitoBundleResult?.result}`);
   } catch (error) {
     console.error(error);
   }
